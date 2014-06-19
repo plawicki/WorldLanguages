4b09b8ae652f00b541504e3c0c179378929ae387
Patryk <patlaw96@gmail.com>
2014-06-20 00:06:25 +0200

    synchronized code

--- app.js
+++ app.js
@@ -3,47 +3,7 @@ var routes = require('./routes');
 var http = require('http');
 var path = require('path');
 var less = require('less-middleware');
-
-var kraje = [];
-var jezyki = [];
-
 var pg = require('pg');
-
-var aDBparams = { host: 'ec2-54-225-101-124.compute-1.amazonaws.com', user: 'mmmwsvrkwfstit', password: 'vlliFqilBOUVyWVY-FZfV50Wa3', database: 'ddehqcgsacci3g',ssl: true };
- 
-var client = new pg.Client(aDBparams);
- 
-client.connect();
- 
-client.query("SELECT * FROM country", function(err, result) {
-    if( result == undefined ){
-        console.log("No results for the query");
-    }else{
-
-        for(i in result.rows)
-        {
-            kraje[i] = result.rows[i];
-        }
-
-        app.set('kraje', { kraje: kraje });
-    }
-    pg.end();
-});
-
-client.query("SELECT * FROM countryLanguage", function(err, result) {
-    if( result == undefined ){
-        console.log("No results for the query");
-    }else{
-
-        for(i in result.rows)
-        {
-            jezyki[i] = result.rows[i];
-        }
-
-        app.set('jezyki', { jezyki: jezyki });
-    }
-    pg.end();
-});
 
 var app = express();
 
@@ -80,11 +40,60 @@ app.configure('development', function () {
     app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/countries', routes.countries);
-app.get('/groupedCountries', routes.groupedCountries);
-app.get(/^\/country\/(\w+)?$/, routes.countryCode);
+var kraje = [];
+var jezyki = [];
 
-http.createServer(app).listen(app.get('port'), function () {
-    console.log("Serwer nasłuchuje na porcie " + app.get('port'));
+var aDBparams = { host: 'ec2-54-225-101-124.compute-1.amazonaws.com', user: 'mmmwsvrkwfstit', password: 'vlliFqilBOUVyWVY-FZfV50Wa3', database: 'ddehqcgsacci3g',ssl: true };
+ 
+var client = new pg.Client(aDBparams);
+ 
+client.connect();
+
+client.query("SELECT * FROM country", function(err, result) {
+    if( result == undefined ){
+        console.log("No results for the query");
+    }else{
+
+        for(i in result.rows)
+        {
+            kraje[i] = result.rows[i];
+        }
+
+        app.set('kraje', { kraje: kraje });
+        nextQuery();
+    }
+    pg.end();
 });
+
+nextQuery = function(){
+
+    client.query("SELECT * FROM countryLanguage", function(err, result) {
+        if( result == undefined ){
+            console.log("No results for the query");
+        }else{
+
+            for(i in result.rows)
+            {
+                jezyki[i] = result.rows[i];
+            }
+
+            app.set('jezyki', { jezyki: jezyki });
+            setUpRouts();
+        }
+        pg.end();
+    });
+
+}
+
+setUpRouts = function(){
+
+    app.get('/', routes.index);
+    app.get('/countries', routes.countries);
+    app.get('/groupedCountries', routes.groupedCountries);
+    app.get(/^\/country\/(\w+)?$/, routes.countryCode);
+
+    http.createServer(app).listen(app.get('port'), function () {
+        console.log("Serwer nasłuchuje na porcie " + app.get('port'));
+    });
+
+}
