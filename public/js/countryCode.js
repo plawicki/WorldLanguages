79e3b29f2e7cda99255b27604a5edf70e6967ecd
Patryk <patlaw96@gmail.com>
2014-06-22 18:43:22 +0200

    piechart + legend

--- public/js/countryCode.js
+++ public/js/countryCode.js
@@ -32,39 +32,59 @@ $(function(){
     	data.push({"label": mojeJezyki[k].language, value: mojeJezyki[k].percentage*100});
     }
     
-    var vis = d3.select(".svg")
-        .append("svg:svg")              //create the SVG element inside the <body>
-        .data([data])                   //associate our data with the document
-            .attr("width", w)           //set the width and height of our visualization (these will be attributes of the <svg> tag
-            .attr("height", h)
-        .append("svg:g")                //make a group to hold our pie chart
-            .attr("transform", "translate(" + r + "," + r + ")")    //move the center of the pie chart from 0, 0 to radius, radius
- 
-    var arc = d3.svg.arc()              //this will create <path> elements for us using arc data
-        .outerRadius(r);
- 
-    var pie = d3.layout.pie()           //this will create arc data for us given a list of values
-        .value(function(d) { return d.value; });    //we must tell it out to access the value of each element in our data array
- 
-    var arcs = vis.selectAll("g.slice")     //this selects all <g> elements with class slice (there aren't any yet)
-        .data(pie)                          //associate the generated pie data (an array of arcs, each having startAngle, endAngle and value properties) 
-        .enter()                            //this will create <g> elements for every "extra" data element that should be associated with a selection. The result is creating a <g> for every object in the data array
-            .append("svg:g")                //create a group to hold each slice (we will have a <path> and a <text> element associated with each slice)
-                .attr("class", "slice");    //allow us to style things in the slices (like text)
- 
-        arcs.append("svg:path")
-                .attr("fill", function(d, i) { return color(i); } ) //set the color for each slice to be chosen from the color function defined above
-                .attr("d", arc);                                    //this creates the actual SVG path using the associated data (pie) with the arc drawing function
- 
-        arcs.append("svg:text")                                     //add a label to each slice
-                .attr("transform", function(d) {                    //set the label's origin to the center of the arc
-                //we have to make sure to set these before calling arc.centroid
-                d.innerRadius = 0;
-                d.outerRadius = r;
-                return "translate(" + arc.centroid(d) + ")";        //this gives us a pair of coordinates like [50, 50]
-            })
-            .attr("text-anchor", "middle")                          //center the text on it's origin
-            .text(function(d, i) { return data[i].label; });        //get the label from our original data array
-        
+	piechart(data);
 
+	function piechart(d){
+        var width = 500,
+	    height = 500,
+	    radius = Math.min(width, height) / 2 - 10;
+
+		var data = [];
+
+		for(var k in d)
+			data.push(d[k].value);
+
+		var color = d3.scale.category20();
+
+		var arc = d3.svg.arc()
+		    .outerRadius(radius);
+
+		var pie = d3.layout.pie();
+
+		var svg = d3.select(".svg").append("svg")
+		    .datum(data)
+		    .attr("width", width)
+		    .attr("height", height)
+		  .append("g")
+		    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+		var arcs = svg.selectAll("g.arc")
+		    .data(pie)
+		  .enter().append("g")
+		    .attr("class", "arc");
+
+		arcs.append("path")
+		    .attr("fill", function(d, i) { return color(i); })
+		  .transition()
+		    .ease("bounce")
+		    .duration(2000)
+		    .attrTween("d", tweenPie)
+		  .transition()
+		    .ease("elastic")
+		    .delay(function(d, i) { return 2000 + i * 50; })
+		    .duration(750);
+
+
+		function tweenPie(b) {
+		  b.innerRadius = 0;
+		  var i = d3.interpolate({startAngle: 0, endAngle: 0}, b);
+		  return function(t) { return arc(i(t)); };
+		}
+		        
+		var textcl = "";
+
+        jQuery.each(d, function(i, val) {
+        	$('tbody').append("<tr><th><div class='legendColor' style='background: "+ color(i)  +"'></th><th>" + val.label + "</th><th>"+ val.value/100 +"</th></tr>");
+        });
+    }
 })
